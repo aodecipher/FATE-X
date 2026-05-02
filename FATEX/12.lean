e1100ae038491657ec021e5898df565e0dba2c8c
Zephyr Fan <zephyrfan@icloud.com>
2026-05-02 11:54:39 -0400

    split: isPrincipalIdealRing_of_quadratic_integer_19 - IsDomain via inferInstance

--- FATEX/12.lean
+++ FATEX/12.lean
@@ -7,6 +7,8 @@ Prove that the ring $\mathbb{Z}[\frac{1+\sqrt{-19}}{2}]$ is a principal ideal do
 -/
 theorem isPrincipalIdealRing_of_quadratic_integer_19 :
     IsPrincipalIdealRing (Algebra.adjoin ℤ {(1 + (Real.sqrt 19) * Complex.I) / 2}) ∧ IsDomain (Algebra.adjoin ℤ {(1 + (Real.sqrt 19) * Complex.I) / 2}) := by
-  sorry
+  refine ⟨?_, ?_⟩
+  · sorry
+  · infer_instance
 
 end Problem12
